da60debade69e68f1064d7f9090442211814ce1d
raxnet <nurvauzan21@gmail.com>
2025-02-05 21:28:02 +0700

    Create config.php

---
+++ config/config.php
@@ -0,0 +1,17 @@
+<?php
+// config.php
+
+// Informasi koneksi database
+$host = 'localhost'; // Ganti dengan host database Anda
+$username = 'root'; // Ganti dengan username database Anda
+$password = 'server'; // Ganti dengan password database Anda
+$dbname = 'panel_db'; // Nama database yang Anda buat
+
+// Membuat koneksi ke database
+$conn = new mysqli($host, $username, $password, $dbname);
+
+// Cek koneksi
+if ($conn->connect_error) {
+    die("Koneksi gagal: " . $conn->connect_error);
+}
+?>
